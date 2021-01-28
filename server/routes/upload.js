5401a5e9d51f8992d3134fbdcc25464c76758d47
weilyuwang <weilyuwang@gmail.com>
2021-01-28 11:49:58 -0500

    :bug: clean up

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const config = require("../config/key");
-const uuid = require("uuid/v1");
 const AWS = require("aws-sdk");
-const path = require("path");
 const mime = require("mime-types");
 
 const s3 = new AWS.S3({
@@ -15,24 +13,9 @@ const s3 = new AWS.S3({
 
 const router = express.Router();
 
-router.get("/awsbuckets", (req, res) => {
-  // Call S3 to list the buckets
-  s3.listBuckets(function (err, data) {
-    if (err) {
-      console.log("Error", err);
-    } else {
-      res.send(data.Buckets);
-    }
-  });
-});
-
 router.get("/signedUrl/:filename", (req, res) => {
   const filename = req.params.filename;
-  const ext = path.extname(filename).split(".").pop();
   const contentType = mime.lookup(filename);
-
-  console.log("file extension is :", ext);
-  console.log("file content type is :", contentType);
 
   const params = {
     Bucket: "quill-editor-blog-demo",
